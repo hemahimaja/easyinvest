33df40adcdac5d67727c61d17b77b69ada38f085
hemahimaja <pallahemahimaja123@gmail.com>
2026-03-10 01:30:50 +0530

    netlify link update

--- easyinvest-backend/index.js
+++ easyinvest-backend/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 // ✅ CORS setup (allow frontend from Netlify + local dev)
 const allowedOrigins = [
   "http://localhost:3000", // local React
-  "https://your-frontend.netlify.app" // 🔁 replace with your actual Netlify URL
+  "https://easyinvestbycai23.netlify.app/" // 🔁 replace with your actual Netlify URL
 ];
 
 app.use(
